192e39a8663e7d74dab03745b9831d8a93f0a0ca
bwang086 <bw-25@outlook.com>
2024-07-24 21:35:39 -0500

    script

--- script2.js
+++ script2.js
@@ -1,7 +1,6 @@
 const width = 960;
 const height = 600;
 
-// Create SVG element
 const svg = d3.select("#map")
     .append("svg")
     .attr("width", width)
@@ -162,3 +161,25 @@ Promise.all([
 }).catch(error => {
     console.error("Error loading the data:", error);
 });
+
+const states = getStates();
+d3.select("#select-state")
+    .selectAll('state-options')
+    .data(entities)
+    .enter()
+    .append('option')
+    .text(function (d) {
+        return d;
+    })
+    .attr("value", function (d) {
+        return d;
+    })
+
+function getStates() {
+    return ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
+    "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
+    "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+    "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
+    "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
+    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
+}
