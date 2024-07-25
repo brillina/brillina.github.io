ec02cc8b59981cda5366819493978039fd0f1465
bwang086 <bw-25@outlook.com>
2024-07-24 21:42:24 -0500

    script2

--- script2.js
+++ script2.js
@@ -105,7 +105,7 @@ Promise.all([
     }
 
     // Create the state selection dropdown
-    const stateSelect = d3.select("#state-select");
+    const stateSelect = d3.select("#select-state");
     Object.values(stateFPtoName).forEach(stateName => {
         stateSelect.append("option")
             .attr("value", stateName)
@@ -161,25 +161,3 @@ Promise.all([
 }).catch(error => {
     console.error("Error loading the data:", error);
 });
-
-const states = getStates();
-d3.select("#select-state")
-    .selectAll('state-options')
-    .data(entities)
-    .enter()
-    .append('option')
-    .text(function (d) {
-        return d;
-    })
-    .attr("value", function (d) {
-        return d;
-    })
-
-function getStates() {
-    return ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware",
-    "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana",
-    "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
-    "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina",
-    "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina",
-    "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
-}
