8ab20d1b8cc4efe92e1ed7b3300eb8223679c123
bwang086 <bw-25@outlook.com>
2024-07-26 21:01:38 -0500

    script2_temp

--- script2_temp.js
+++ script2_temp.js
@@ -1,90 +1,53 @@
-d3.csv('data/mask_averages.csv', function (data) {
-    // Variables
-    var body = d3.select('body')
-      var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-      var h = 500 - margin.top - margin.bottom
-      var w = 500 - margin.left - margin.right
-      var formatPercent = d3.format('.2%')
-      // Scales
-    var colorScale = d3.scale.category20()
-    var xScale = d3.scale.linear()
-      .domain([
-          d3.min([0,d3.min(data,function (d) { return d.fips })]),
-          d3.max([0,d3.max(data,function (d) { return d.fips })])
-          ])
-      .range([0,w])
-    var yScale = d3.scale.linear()
-      .domain([
-          d3.min([0,d3.min(data,function (d) { return d.weightedAverage })]),
-          d3.max([0,d3.max(data,function (d) { return d.weightedAverage })])
-          ])
-      .range([h,0])
-      // SVG
-      var svg = body.append('svg')
-          .attr('height',h + margin.top + margin.bottom)
-          .attr('width',w + margin.left + margin.right)
-        .append('g')
-          .attr('transform','translate(' + margin.left + ',' + margin.top + ')')
-      // X-axis
-      var xAxis = d3.svg.axis()
-        .scale(xScale)
-        .tickFormat(formatPercent)
-        .ticks(5)
-        .orient('bottom')
-    // Y-axis
-      var yAxis = d3.svg.axis()
-        .scale(yScale)
-        .tickFormat(formatPercent)
-        .ticks(5)
-        .orient('left')
-    // Circles
-    var circles = svg.selectAll('circle')
+// Set up margins and dimensions
+const margin = {top: 20, right: 30, bottom: 30, left: 40};
+const width = 800 - margin.left - margin.right;
+const height = 600 - margin.top - margin.bottom;
+
+// Append SVG to the body and set up the chart area
+const svg = d3.select("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Define the scales
+const xScale = d3.scaleLinear().range([0, width]);
+const yScale = d3.scaleLinear().range([height, 0]);
+
+// Define the axes
+const xAxis = d3.axisBottom(xScale);
+const yAxis = d3.axisLeft(yScale);
+
+// Load the CSV data
+d3.csv("mask_averages.csv").then(data => {
+    // Parse the data
+    data.forEach(d => {
+        d.weightedAverage = +d.weightedAverage;
+        d.cases = +d.cases.replace(/,/g, ''); // Remove commas in case numbers
+    });
+
+    // Set domains for scales
+    xScale.domain(d3.extent(data, d => d.weightedAverage)).nice();
+    yScale.domain(d3.extent(data, d => d.cases)).nice();
+
+    // Append axes
+    svg.append("g")
+        .attr("class", "x-axis")
+        .attr("transform", `translate(0,${height})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("class", "y-axis")
+        .call(yAxis);
+
+    // Append dots
+    svg.selectAll(".dot")
         .data(data)
-        .enter()
-      .append('circle')
-        .attr('cx',function (d) { return xScale(d.fips) })
-        .attr('cy',function (d) { return yScale(d.weightedAverage) })
-        .attr('r','10')
-        .attr('stroke','black')
-        .attr('stroke-width',1)
-        .attr('fill',function (d,i) { return colorScale(i) })
-        .on('mouseover', function () {
-          d3.select(this)
-            .transition()
-            .duration(500)
-            .attr('r',20)
-            .attr('stroke-width',3)
-        })
-        .on('mouseout', function () {
-          d3.select(this)
-            .transition()
-            .duration(500)
-            .attr('r',10)
-            .attr('stroke-width',1)
-        })
-        
-    // X-axis
-    svg.append('g')
-        .attr('class','axis')
-        .attr('transform', 'translate(0,' + h + ')')
-        .call(xAxis)
-      .append('text') // X-axis Label
-        .attr('class','label')
-        .attr('y',-10)
-        .attr('x',w)
-        .attr('dy','.71em')
-        .style('text-anchor','end')
-        .text('Annualized Standard Deviation')
-    // Y-axis
-    svg.append('g')
-        .attr('class', 'axis')
-        .call(yAxis)
-      .append('text') // y-axis Label
-        .attr('class','label')
-        .attr('transform','rotate(-90)')
-        .attr('x',0)
-        .attr('y',5)
-        .attr('dy','.71em')
-        .style('text-anchor','end')
-        .text('Annualized Return')
-  })
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("cx", d => xScale(d.weightedAverage))
+        .attr("cy", d => yScale(d.cases))
+        .attr("r", 5)
+        .append("title")
+          .text(d => d.fips); // Tooltip with fips
+});
