c0a74736975ff3a2cd32f916e5c31be015f1cd61
bwang086 <bw-25@outlook.com>
2024-08-07 16:08:29 -0500

    script2 fix

--- script2_temp.js
+++ script2_temp.js
@@ -70,108 +70,129 @@ const tooltip = d3.select("body").append("div")
             .attr("transform", "rotate(-90)")
             .text("Cases");
     
-        function updateScatterplot(selectedState) {
-            const hideZeroCases = d3.select("#hide-zero-cases").property("checked");
-            const filteredData = selectedState === "all" ? data : data.filter(d => d.state === selectedState);
-            const displayData = hideZeroCases ? filteredData.filter(d => d.cases > 0) : filteredData;
-    
-            svg.selectAll(".dot").remove();
-    
-            svg.selectAll(".dot")
-                .data(displayData)
-              .enter().append("circle")
-                .attr("class", "dot")
-                .attr("cx", d => xScale(d.weightedAverage))
-                .attr("cy", d => yScale(d.cases))
-                .attr("r", 5)
-                .attr("fill", d => colorScale(d.state))
-                .on("mouseover", (event, d) => {
-                    tooltip.transition().duration(200).style("opacity", .9);
-                    tooltip.html(`<strong>${d.county}, ${d.state}</strong><br>
-                                  Cases: ${d.cases}<br>
-                                  Deaths: ${d.deaths}<br>
-                                  Weighted Avg: ${d.weightedAverage}<br>
-                                  NEVER: ${d.NEVER}<br>
-                                  RARELY: ${d.RARELY}<br>
-                                  SOMETIMES: ${d.SOMETIMES}<br>
-                                  FREQUENTLY: ${d.FREQUENTLY}<br>
-                                  ALWAYS: ${d.ALWAYS}`)
-                        .style("left", (event.pageX + 5) + "px")
-                        .style("top", (event.pageY - 28) + "px");
-    
-                    const originalColor = d3.color(colorScale(d.state));
-                    const hoverColor = originalColor.brighter(2);
-                    d3.select(event.currentTarget)
-                      .attr("r", 8)
-                      .attr("fill", hoverColor)
-                      .attr("stroke", "black")
-                      .attr("stroke-width", 2);
-                })
-                .on("mouseout", (event, d) => {
-                    tooltip.transition().duration(500).style("opacity", 0);
-    
-                    d3.select(event.currentTarget)
-                      .attr("r", 5)
-                      .attr("fill", colorScale(d.state))
-                      .attr("stroke", null)
-                      .attr("stroke-width", null);
-                });
-    
-            const mower = data.find(d => d.county === "Mower" && d.state === "Minnesota");
-            const kenedy = data.find(d => d.county === "Kenedy" && d.state === "Texas");
-    
-            if (mower) {
-                svg.append("text")
-                    .attr("x", xScale(mower.weightedAverage) + 10)
-                    .attr("y", yScale(mower.cases) - 30)
-                    .attr("class", "annotation")
-                    .text(`Mower, MN`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
-    
-                svg.append("text")
-                    .attr("x", xScale(mower.weightedAverage) + 10)
-                    .attr("y", yScale(mower.cases) - 15)
-                    .attr("class", "annotation")
-                    .text(`Mask: ${mower.NEVER + mower.RARELY + mower.SOMETIMES + mower.FREQUENTLY + mower.ALWAYS}`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
-    
-                svg.append("text")
-                    .attr("x", xScale(mower.weightedAverage) + 10)
-                    .attr("y", yScale(mower.cases))
-                    .attr("class", "annotation")
-                    .text(`Cases: ${mower.cases}`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
+            function updateScatterplot(selectedState) {
+                const hideZeroCases = d3.select("#hide-zero-cases").property("checked");
+                const filteredData = selectedState === "all" ? data : data.filter(d => d.state === selectedState);
+                const displayData = hideZeroCases ? filteredData.filter(d => d.cases > 0) : filteredData;
+            
+                svg.selectAll(".dot").remove();
+            
+                svg.selectAll(".dot")
+                    .data(displayData)
+                  .enter().append("circle")
+                    .attr("class", "dot")
+                    .attr("cx", d => xScale(d.weightedAverage))
+                    .attr("cy", d => yScale(d.cases))
+                    .attr("r", 5)
+                    .attr("fill", d => colorScale(d.state))
+                    .on("mouseover", (event, d) => {
+                        tooltip.transition().duration(200).style("opacity", .9);
+                        tooltip.html(`<strong>${d.county}, ${d.state}</strong><br>
+                                      Cases: ${d.cases}<br>
+                                      Deaths: ${d.deaths}<br>
+                                      Weighted Avg: ${d.weightedAverage}<br>
+                                      NEVER: ${d.NEVER}<br>
+                                      RARELY: ${d.RARELY}<br>
+                                      SOMETIMES: ${d.SOMETIMES}<br>
+                                      FREQUENTLY: ${d.FREQUENTLY}<br>
+                                      ALWAYS: ${d.ALWAYS}`)
+                            .style("left", (event.pageX + 5) + "px")
+                            .style("top", (event.pageY - 28) + "px");
+            
+                        const originalColor = d3.color(colorScale(d.state));
+                        const hoverColor = originalColor.brighter(2);
+                        d3.select(event.currentTarget)
+                          .attr("r", 8)
+                          .attr("fill", hoverColor)
+                          .attr("stroke", "black")
+                          .attr("stroke-width", 2);
+                    })
+                    .on("mouseout", (event, d) => {
+                        tooltip.transition().duration(500).style("opacity", 0);
+            
+                        d3.select(event.currentTarget)
+                          .attr("r", 5)
+                          .attr("fill", colorScale(d.state))
+                          .attr("stroke", null)
+                          .attr("stroke-width", null);
+                    });
+            
+                svg.selectAll(".annotation").remove();
+                svg.selectAll(".annotation-line").remove();
+            
+                const mower = data.find(d => d.county === "Mower" && d.state === "Minnesota");
+                const kenedy = data.find(d => d.county === "Kenedy" && d.state === "Texas");
+            
+                if (mower) {
+                    svg.append("text")
+                        .attr("x", xScale(mower.weightedAverage) + 20)
+                        .attr("y", yScale(mower.cases) - 30)
+                        .attr("class", "annotation")
+                        .text(`Mower, MN`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+            
+                    svg.append("text")
+                        .attr("x", xScale(mower.weightedAverage) + 20)
+                        .attr("y", yScale(mower.cases) - 15)
+                        .attr("class", "annotation")
+                        .text(`Mask: ${mower.NEVER + mower.RARELY + mower.SOMETIMES + mower.FREQUENTLY + mower.ALWAYS}`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+            
+                    svg.append("text")
+                        .attr("x", xScale(mower.weightedAverage) + 20)
+                        .attr("y", yScale(mower.cases))
+                        .attr("class", "annotation")
+                        .text(`Cases: ${mower.cases}`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+                }
+            
+                if (kenedy) {
+                    const kenedyX = xScale(kenedy.weightedAverage);
+                    const kenedyY = yScale(kenedy.cases);
+            
+                    const annotationX = kenedyX + 20;
+                    const annotationY1 = kenedyY - 30;
+                    const annotationY2 = kenedyY - 15;
+                    const annotationY3 = kenedyY;
+            
+                    svg.append("line")
+                        .attr("class", "annotation-line")
+                        .attr("x1", kenedyX)
+                        .attr("y1", kenedyY)
+                        .attr("x2", annotationX - 5)
+                        .attr("y2", annotationY1 + 15)
+                        .attr("stroke", "black")
+                        .attr("stroke-width", 1);
+            
+                    svg.append("text")
+                        .attr("x", annotationX)
+                        .attr("y", annotationY1)
+                        .attr("class", "annotation")
+                        .text(`Kenedy, TX`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+            
+                    svg.append("text")
+                        .attr("x", annotationX)
+                        .attr("y", annotationY2)
+                        .attr("class", "annotation")
+                        .text(`Mask: ${kenedy.NEVER + kenedy.RARELY + kenedy.SOMETIMES + kenedy.FREQUENTLY + kenedy.ALWAYS}`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+            
+                    svg.append("text")
+                        .attr("x", annotationX)
+                        .attr("y", annotationY3)
+                        .attr("class", "annotation")
+                        .text(`Cases: ${kenedy.cases}`)
+                        .style("font-size", "12px")
+                        .style("fill", "black");
+                }
             }
-    
-            if (kenedy) {
-                svg.append("text")
-                    .attr("x", xScale(kenedy.weightedAverage) + 150)
-                    .attr("y", yScale(kenedy.cases) - 20)
-                    .attr("class", "annotation")
-                    .text(`Kenedy, TX`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
-    
-                svg.append("text")
-                    .attr("x", xScale(kenedy.weightedAverage) + 150)
-                    .attr("y", yScale(kenedy.cases) - 5)
-                    .attr("class", "annotation")
-                    .text(`Mask: ${kenedy.NEVER + kenedy.RARELY + kenedy.SOMETIMES + kenedy.FREQUENTLY + kenedy.ALWAYS}`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
-    
-                svg.append("text")
-                    .attr("x", xScale(kenedy.weightedAverage) + 150)
-                    .attr("y", yScale(kenedy.cases) + 10)
-                    .attr("class", "annotation")
-                    .text(`Cases: ${kenedy.cases}`)
-                    .style("font-size", "12px")
-                    .style("fill", "black");
-            }
-        }
+            
     
         updateScatterplot("all");
     
